d821edbf7fc7a941bff5f579c5dbac5200f843dc
unknown <Talak@.(none)>
2011-10-24 16:58:59 -0400

    Initial Import

---
+++ src/com/example/umainehabapp/umainehabappActivity.java
@@ -0,0 +1,13 @@
+package com.example.umainehabapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class umainehabappActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

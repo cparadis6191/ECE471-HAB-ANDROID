18ff62ead64547427b736ca5fbf171a015eba319
unknown <Talak@.(none)>
2011-10-25 11:47:55 -0400

    Added helium .xml and activity

---
+++ src/com/example/umainehabapp/heliumActivity.java
@@ -0,0 +1,15 @@
+package com.example.umainehabapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class heliumActivity extends Activity {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helium);
+        
+        
+    }
+}
--- src/com/example/umainehabapp/umainehabappActivity.java
+++ src/com/example/umainehabapp/umainehabappActivity.java
@@ -1,13 +1,24 @@
 package com.example.umainehabapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class umainehabappActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.mainview);
+        
+        final Button btnhelium = (Button) findViewById(R.id.btnhelium);
+        btnhelium.setOnClickListener(new View.OnClickListener() { //button listener to launch helium activity
+        	public void onClick(View v) {
+        		Intent intenthelium = new Intent(umainehabappActivity.this, heliumActivity.class);
+        		startActivity(intenthelium);
+        	}
+        });
     }
 }

1025602464e688527c5d862ce4ea290acf26cb3a
unknown <Talak@.(none)>
2011-11-01 12:09:39 -0400

    Fixed helium calculator text box hints

--- gen/com/example/umainehabapp/R.java
+++ gen/com/example/umainehabapp/R.java
@@ -18,9 +18,11 @@ public final class R {
     public static final class id {
         public static final int BalloonType=0x7f050001;
         public static final int RelativeLayout1=0x7f050000;
-        public static final int btnhelium=0x7f050004;
+        public static final int btnhelium=0x7f050006;
         public static final int editText1=0x7f050002;
-        public static final int relativeLayoutMain=0x7f050003;
+        public static final int editText2=0x7f050003;
+        public static final int editText3=0x7f050004;
+        public static final int relativeLayoutMain=0x7f050005;
     }
     public static final class layout {
         public static final int helium=0x7f030000;
@@ -29,6 +31,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int ascentratehint=0x7f040002;
+        public static final int burstaltitudehint=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int payloadweighthint=0x7f040004;
     }
 }

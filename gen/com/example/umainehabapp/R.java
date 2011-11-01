a799c7b3d4182e5db1d2f1105b98caa4d87d0dd8
unknown <Talak@.(none)>
2011-11-01 12:18:17 -0400

    Fixed string and ids to be less ambiguous

--- gen/com/example/umainehabapp/R.java
+++ gen/com/example/umainehabapp/R.java
@@ -16,13 +16,15 @@ public final class R {
         public static final int menubackground=0x7f020002;
     }
     public static final class id {
-        public static final int BalloonType=0x7f050001;
+        public static final int BalloonType=0x7f050003;
         public static final int RelativeLayout1=0x7f050000;
-        public static final int btnhelium=0x7f050006;
-        public static final int editText1=0x7f050002;
-        public static final int editText2=0x7f050003;
-        public static final int editText3=0x7f050004;
-        public static final int relativeLayoutMain=0x7f050005;
+        public static final int btnhelium=0x7f050008;
+        public static final int editText2=0x7f050006;
+        public static final int editTextascentrate=0x7f050005;
+        public static final int editTextballoontype=0x7f050004;
+        public static final int editTextpayloadweight=0x7f050002;
+        public static final int relativeLayoutMain=0x7f050007;
+        public static final int spinnerBalloonType=0x7f050001;
     }
     public static final class layout {
         public static final int helium=0x7f030000;
@@ -31,9 +33,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int ascentratehint=0x7f040002;
-        public static final int burstaltitudehint=0x7f040003;
         public static final int hello=0x7f040000;
-        public static final int payloadweighthint=0x7f040004;
+        public static final int hintascentrate=0x7f040002;
+        public static final int hintburstaltitude=0x7f040003;
+        public static final int hintpayloadweight=0x7f040004;
     }
 }

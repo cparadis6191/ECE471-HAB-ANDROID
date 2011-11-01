c94698bf9b8f0dde6920b001d62ff1b212fe1971
Ian LaForge <ian.laforge@umit.maine.edu>
2011-11-01 11:52:01 -0400

    changed more layout things

--- gen/com/example/umainehabapp/R.java
+++ gen/com/example/umainehabapp/R.java
@@ -16,10 +16,11 @@ public final class R {
         public static final int menubackground=0x7f020002;
     }
     public static final class id {
+        public static final int BalloonType=0x7f050001;
         public static final int RelativeLayout1=0x7f050000;
-        public static final int btnhelium=0x7f050003;
-        public static final int relativeLayoutMain=0x7f050002;
-        public static final int spinner1=0x7f050001;
+        public static final int btnhelium=0x7f050004;
+        public static final int editText1=0x7f050002;
+        public static final int relativeLayoutMain=0x7f050003;
     }
     public static final class layout {
         public static final int helium=0x7f030000;

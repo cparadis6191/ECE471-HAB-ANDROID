354178fe7af3dfcabda75aa25bf45c52736b6ac8
unknown <Sayier@Sayier-PC.(none)>
2011-11-08 00:08:24 -0500

    Created xml's for map view and screen into map view.

--- gen/com/example/umainehabapp/R.java
+++ gen/com/example/umainehabapp/R.java
@@ -18,14 +18,22 @@ public final class R {
     public static final class id {
         public static final int BalloonType=0x7f050001;
         public static final int RelativeLayout1=0x7f050000;
-        public static final int btnhelium=0x7f050004;
+        public static final int btnhelium=0x7f050006;
+        public static final int btnpath=0x7f050007;
+        public static final int btntomap=0x7f05000b;
         public static final int editText1=0x7f050002;
-        public static final int relativeLayoutMain=0x7f050003;
+        public static final int editText2=0x7f050003;
+        public static final int editText3=0x7f050004;
+        public static final int mainlayout=0x7f050008;
+        public static final int mapview=0x7f050009;
+        public static final int relativeLayout1=0x7f05000a;
+        public static final int relativeLayoutMain=0x7f050005;
     }
     public static final class layout {
         public static final int helium=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int mainview=0x7f030002;
+        public static final int mainview=0x7f030001;
+        public static final int pathmap=0x7f030002;
+        public static final int pathparam=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
